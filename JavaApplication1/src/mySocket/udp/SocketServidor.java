3a599d13684b78b0b89afe34abef9cd2d30eb527
André Luíz <andreluizwandscheer@gmail.com>
2025-08-25 13:33:29 -0300

    Add files via upload

---
+++ JavaApplication1/src/mySocket/udp/SocketServidor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mySocket.udp;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+/**
+ *
+ * @author aluno
+ */
+public class SocketServidor {
+    public static void main(String[] args) {
+        try{
+            DatagramSocket servidor = new DatagramSocket(1026);
+                            
+            byte[] mensagem = new byte[256];
+            
+            DatagramPacket pacote = new DatagramPacket(mensagem,mensagem.length);
+            
+             servidor.receive(pacote);
+             
+             System.out.println(new String(pacote.getData()).trim());
+             
+             servidor.close();
+        }catch(Exception e){
+            System.out.println("Ocorreu erro:"+e.getMessage());
+        }
+    }
+}
