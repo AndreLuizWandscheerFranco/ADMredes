8ba7c804cd23382de2ebf03d85ccaf088b0f88cb
André Luíz <andreluizwandscheer@gmail.com>
2025-09-08 13:38:10 -0300

    Add files via upload

---
+++ JavaApplication1/src/SocketThread/tcp/ServerTCPthread.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SocketThread.tcp;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author aluno
+ */
+public class ServerTCPthread {
+    private ServerSocket server = null;
+    private HashSet<Socket> clients = new HashSet<Socket>();
+    
+    public ServerTCPthread(int port)throws IOException{
+        this.server = new ServerSocket(port);
+    }
+    
+    public void execute(){
+        while(true){
+            try{
+                Socket client = this.server.accept();
+                
+                
+            }catch(IOException e){
+                System.out.println("Ocorreu um erro em:"+e.getMessage());
+            }
+        }
+    }
+}
